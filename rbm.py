Fix training reconstruction error calculation

rbm.py
@@ -10,7 +10,7 @@ class RBM:
         num_hidden,
         gibbs_steps,
         learning_rate=1e-3,
-        momentum_coefficient=0.9,
+        momentum_coefficient=0.5,
         weight_decay=1e-4,
         device=None
     ):
@@ -184,7 +184,8 @@ class RBM:
         self.weights -= self.weights * self.weight_decay
 
         # Compute reconstruction error (L1 norm since values are binary)
-        error = torch.sum(torch.abs(input_data - visible_values))
+        reconstruction = self.sample_visible(self.sample_hidden(input_data))
+        error = torch.sum(torch.abs(input_data - reconstruction))
 
         return error
 

train_mnist.py
@@ -9,11 +9,11 @@ from rbm import RBM
 BATCH_SIZE = 64
 VISIBLE_UNITS = 784 # 28 x 28 images
 HIDDEN_UNITS = 128
-CD_K = 1
+CD_K = 2
 LEARNING_RATE = 1e-3
-MOMENTUM_COEFFICIENT = 0.9
+MOMENTUM_COEFFICIENT = 0.5
 WEIGHT_DECAY = 1e-4
-EPOCHS = 20
+EPOCHS = 100
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 